421d54644509e5dc0a4adffa3c0359b2394af78c
LordGhostX <solomonghost9@gmail.com>
2021-02-23 09:39:30 +0100

    added Kata

---
+++ Extras/Kata.java
@@ -0,0 +1,5 @@
+public class Kata {
+	public double calcAverage(int firstNumber, int secondNumber, int thirdNumber) {
+		return (firstNumber + secondNumber + thirdNumber) / 3.0;
+	}
+}
---
+++ Extras/KataTest.java
@@ -0,0 +1,7 @@
+public class KataTest {
+	public static void main(String[] args) {
+		Kata kata = new Kata();
+
+		System.out.println("The average of the numbers are: " + kata.calcAverage(3, 4, 5));
+	}	
+}
