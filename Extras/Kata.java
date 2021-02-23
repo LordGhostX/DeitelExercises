eae9a2e81186dba14a4edeef4e4480d365093cf7
LordGhostX <solomonghost9@gmail.com>
2021-02-23 10:46:53 +0100

    updated Kata

--- Extras/Kata.java
+++ Extras/Kata.java
@@ -1,5 +1,23 @@
 public class Kata {
 	public double calcAverage(int firstNumber, int secondNumber, int thirdNumber) {
-		return (firstNumber + secondNumber + thirdNumber) / 3.0;
+		double result = (firstNumber + secondNumber + thirdNumber) / 3.0;
+		return result;
+	}
+
+	public boolean isEven(int number) {
+		boolean isEven = (number % 2 == 0);
+		return isEven;
+	}
+
+	public String calcGrade(int score) {
+		if (score >= 90) {
+			return "A";
+		} else if (score >= 80) {
+			return "B";
+		} else if (score >= 70) {
+			return "C";
+		} else {
+			return "D";
+		}
 	}
 }
--- Extras/KataTest.java
+++ Extras/KataTest.java
@@ -3,5 +3,9 @@ public class KataTest {
 		Kata kata = new Kata();
 
 		System.out.println("The average of the numbers are: " + kata.calcAverage(3, 4, 5));
+
+		System.out.println("Is 10 Even: " + kata.isEven(10));
+
+		System.out.println("The grade for 79 is " + kata.calcGrade(79));
 	}	
 }
